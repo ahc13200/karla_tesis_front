fb5e3ad335fb2f638fbdafbdc9833e95c4ea4489
ahc13 <ahc13200@gmail.com>
2023-08-02 11:41:52 -0400

    read .xpress

--- lib/pages/home/components/ChooseFile.vue
+++ lib/pages/home/components/ChooseFile.vue
@@ -10,7 +10,7 @@
         const reader = new FileReader();
         if (file.value.name.includes(".xpress") || file.value.name.includes(".txt")) {
             reader.onload = (res) => {
-                content.value = res.target.result;
+                content.value = res.target.result.split(",");
             };
             reader.onerror = (err) => console.log(err);
             reader.readAsText(file.value);
